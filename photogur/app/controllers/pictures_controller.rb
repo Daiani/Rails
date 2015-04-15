516c2e072adffa4622686e4d946a29dbd8fb2eab
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-04-15 17:40:11 -0400

    adding feature to delete pic

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -22,13 +22,34 @@ class PicturesController < ApplicationController
 		@picture = Picture.find(params[:id])
 	end
 
+	def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+    end
+  end
+end
+
 private
 
 	def picture_params
 		params.require(:picture).permit(:artist, :title, :url)
 	end
 
-end
+
 
 
 
--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -7,7 +7,15 @@ get 'pictures' => 'pictures#index'
 post 'pictures' => 'pictures#create'
 get 'pictures/new' => 'pictures#new'
  
-get 'picture/:id' => 'pictures#show'  #as: 'picture'
+get 'picture/:id' => 'pictures#show' 
+
+get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+  root 'pictures#index'
+end
 
 
 
